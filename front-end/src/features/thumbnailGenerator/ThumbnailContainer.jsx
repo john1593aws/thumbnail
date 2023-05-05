e57d90d4a9010153fd36958e26141235d86e940d
John Alvarez <john1593aws@gmail.com>
2023-05-05 01:22:20 -0500

    fetched items for store

--- front-end/src/features/thumbnailGenerator/ThumbnailContainer.jsx
+++ front-end/src/features/thumbnailGenerator/ThumbnailContainer.jsx
@@ -1,10 +1,19 @@
-import React from "react";
-import { Container, Box } from "@mui/material";
-import { addItem, fetchItems } from "./thumbnailSlice";
-import ItemForm from "./ItemForm";
-import ItemTable from "./ItemTable";
+import { Container, Box } from '@mui/material';
+import { addItem, fetchItems } from './thumbnailSlice';
+import ItemForm from './ItemForm';
+import ItemTable from './ItemTable';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 const ThumbnailContainer = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    async function fetchItemsEffect() {
+      dispatch(fetchItems());
+    }
+    fetchItemsEffect();
+  }, [dispatch]);
+
   return (
     <Container>
       <Box m="auto" maxWidth="sm">
--- front-end/src/features/thumbnailGenerator/thumbnailSlice.js
+++ front-end/src/features/thumbnailGenerator/thumbnailSlice.js
@@ -1,40 +1,50 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const path = 'http://localhost:5000/items';
 
 const initialState = {
   items: [],
-  status: "idle",
+  status: 'idle',
 };
 
-export const fetchItems = createAsyncThunk("thumbnail/fetchItems", async () => {
-  //do stuff
-
-  return null;
+export const fetchItems = createAsyncThunk('thumbnail/fetchItems', async () => {
+  console.log('yo yo');
+  try {
+    const items = await axios.get(path);
+    console.log({ items });
+    return items;
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-export const addItem = createAsyncThunk("thumbnail/addItem", async (item) => {
-  //do stuff
-
-  return null;
+export const addItem = createAsyncThunk('thumbnail/addItem', async (item) => {
+  try {
+    await axios.post(path, item);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 export const thumbnailSlice = createSlice({
-  name: "thumbnail",
+  name: 'thumbnail',
   initialState,
 
   extraReducers: (builder) => {
     builder
       .addCase(fetchItems.pending, (state) => {
-        state.status = "loading";
+        state.status = 'loading';
       })
       .addCase(fetchItems.fulfilled, (state, action) => {
-        state.status = "idle";
-        state.items += action.payload;
+        state.status = 'idle';
+        state.items = action.payload.data[0];
       })
       .addCase(addItem.pending, (state) => {
-        state.status = "loading";
+        state.status = 'loading';
       })
       .addCase(addItem.fulfilled, (state, action) => {
-        state.items += [action.payload, ...state.items];
+        state.status = 'idle';
       });
   },
 });
