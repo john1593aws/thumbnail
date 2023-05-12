f73d7acb092dda492d7418fbb399727b2b17f189
John Alvarez <john1593aws@gmail.com>
2023-05-12 16:03:18 -0500

    can view pdf with strange title

--- front-end/src/features/items/ItemsTable.jsx
+++ front-end/src/features/items/ItemsTable.jsx
@@ -16,6 +16,28 @@ import { useSelector } from 'react-redux';
 const ItemsTable = () => {
   const [items] = useSelector((state) => [state.itemsSlice.items]);
 
+  const arrayBufferToBase64 = (buffer) => {
+    let binary = '';
+    let bytes = new Uint8Array(buffer);
+    let len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+  };
+
+  function debugBase64(base64URL) {
+    const fileWindow = window.open();
+    fileWindow.document.write(
+      '<title>Visualisation</title>' +
+        '<body style="overflow: hidden; margin: 0">' +
+        '<object width="100%" width="-webkit-fill-available" height="100%" height="-webkit-fill-available" type="application/pdf" data="' +
+        encodeURI(base64URL) +
+        '"></object>' +
+        '</body>'
+    );
+  }
+
   return (
     <Card
       sx={{
@@ -35,12 +57,18 @@ const ItemsTable = () => {
             </TableHead>
             <TableBody>
               {items.map((row, i) => {
+                const fileStr = arrayBufferToBase64(row.file.buffer);
+                console.log(fileStr);
                 return (
                   <TableRow key={i}>
                     <TableCell align="left">{row.id}</TableCell>
                     <TableCell align="left">{row.name}</TableCell>
                     <TableCell align="right">
-                      <IconButton>
+                      <IconButton
+                        onClick={() => {
+                          debugBase64(`data:application/pdf;base64,${fileStr}`);
+                        }}
+                      >
                         <FindInPageIcon color="disabled" />
                       </IconButton>
                     </TableCell>
